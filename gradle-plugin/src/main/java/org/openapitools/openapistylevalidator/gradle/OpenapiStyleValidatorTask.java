a9c4a3d39787a3f25bedf4f33a61f82d208f01ef
Jeremie Bresson <jeremie.bresson@unblu.com>
2019-11-15 11:36:14 +0100

    XXXY

--- gradle-plugin/src/main/java/org/openapitools/openapistylevalidator/gradle/OpenapiStyleValidatorTask.java
+++ gradle-plugin/src/main/java/org/openapitools/openapistylevalidator/gradle/OpenapiStyleValidatorTask.java
@@ -1,12 +1,30 @@
 package org.openapitools.openapistylevalidator.gradle;
 
 import org.gradle.api.DefaultTask;
-import org.gradle.api.InvalidUserDataException;
 import org.gradle.api.tasks.TaskAction;
-import org.gradle.api.tasks.options.Option;
+import org.openapitools.openapistylevalidator.ValidatorParameters;
+import org.openapitools.openapistylevalidator.ValidatorParameters.NamingStrategy;
 
 public class OpenapiStyleValidatorTask extends DefaultTask {
 
+    private boolean validateInfoLicense = true;
+    private boolean validateInfoDescription = true;
+    private boolean validateInfoContact = true;
+
+    private boolean validateOperationOperationId = true;
+    private boolean validateOperationDescription = true;
+    private boolean validateOperationTag = true;
+    private boolean validateOperationSummary = true;
+
+    private boolean validateModelPropertiesExample = true;
+    private boolean validateModelNoLocalDef = true;
+
+    private boolean validateNaming = true;
+    private boolean ignoreHeaderXNaming = true;
+    private NamingStrategy pathNamingStrategy = NamingStrategy.HyphenCase;
+    private NamingStrategy parameterNamingStrategy = NamingStrategy.CamelCase;
+    private NamingStrategy propertyNamingStrategy = NamingStrategy.CamelCase;
+    
     public OpenapiStyleValidatorTask() {
         this.setGroup("Verification");
         this.setDescription("Validate that OpenAPI files against style rules");
@@ -14,24 +32,41 @@ public class OpenapiStyleValidatorTask extends DefaultTask {
 
     @TaskAction
     public void execute() {
-        // OpenAPIParser openApiParser = new OpenAPIParser();
-        // ParseOptions parseOptions = new ParseOptions();
-        // parseOptions.setResolve(true);
-
-        // SwaggerParseResult parserResult = openApiParser.readLocation(optionManager.getSource(commandLine), null, parseOptions);
-        // io.swagger.v3.oas.models.OpenAPI swaggerOpenAPI = parserResult.getOpenAPI();
-
-        // org.eclipse.microprofile.openapi.models.OpenAPI openAPI = SwAdapter.toOpenAPI(swaggerOpenAPI);
-        // OpenApiSpecStyleValidator openApiSpecStyleValidator = new OpenApiSpecStyleValidator(openAPI);
-
-        // ValidatorParameters parameters = createValidatorParameters());
-
-        // outputUtils.printResults(openApiSpecStyleValidator.validate(parameters));
-
         System.out.println("Hello from plugin 'org.openapitools.openapistylevalidator'");
+
+//         OpenAPIParser openApiParser = new OpenAPIParser();
+//         ParseOptions parseOptions = new ParseOptions();
+//         parseOptions.setResolve(true);
+//
+//         SwaggerParseResult parserResult = openApiParser.readLocation(optionManager.getSource(commandLine), null, parseOptions);
+//         io.swagger.v3.oas.models.OpenAPI swaggerOpenAPI = parserResult.getOpenAPI();
+//
+//        // org.eclipse.microprofile.openapi.models.OpenAPI openAPI = SwAdapter.toOpenAPI(swaggerOpenAPI);
+//         OpenApiSpecStyleValidator openApiSpecStyleValidator = new OpenApiSpecStyleValidator(openAPI);
+//
+//        // ValidatorParameters parameters = createValidatorParameters());
+//
+//        openApiSpecStyleValidator.validate(parameters)
+
     }
 
-    // public ValidatorParameters createValidatorParameters() {
-    // }
+     public ValidatorParameters createValidatorParameters() {
+        ValidatorParameters parameters = new ValidatorParameters();
+        parameters.setValidateInfoLicense(validateInfoLicense);
+        parameters.setValidateInfoDescription(validateInfoDescription);
+        parameters.setValidateInfoContact(validateInfoContact);
+        parameters.setValidateOperationOperationId(validateOperationOperationId);
+        parameters.setValidateOperationDescription(validateOperationDescription);
+        parameters.setValidateOperationTag(validateOperationTag);
+        parameters.setValidateOperationSummary(validateOperationSummary);
+        parameters.setValidateModelPropertiesExample(validateModelPropertiesExample);
+        parameters.setValidateModelNoLocalDef(validateModelNoLocalDef);
+        parameters.setValidateNaming(validateNaming);
+        parameters.setIgnoreHeaderXNaming(ignoreHeaderXNaming);
+        parameters.setPathNamingStrategy(pathNamingStrategy);
+        parameters.setParameterNamingStrategy(parameterNamingStrategy);
+        parameters.setPropertyNamingStrategy(propertyNamingStrategy);
+        return parameters;
+     }
 
 }
