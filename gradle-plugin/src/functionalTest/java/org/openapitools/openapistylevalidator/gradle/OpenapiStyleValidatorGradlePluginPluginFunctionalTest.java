815c73e7c035505687c5536c4ae1664219a9ad0e
Jeremie Bresson <jeremie.bresson@unblu.com>
2019-11-19 07:13:59 +0100

    YYY

--- gradle-plugin/src/functionalTest/java/org/openapitools/openapistylevalidator/gradle/OpenapiStyleValidatorGradlePluginPluginFunctionalTest.java
+++ gradle-plugin/src/functionalTest/java/org/openapitools/openapistylevalidator/gradle/OpenapiStyleValidatorGradlePluginPluginFunctionalTest.java
@@ -1,14 +1,13 @@
 package org.openapitools.openapistylevalidator.gradle;
 
+import org.gradle.testkit.runner.GradleRunner;
+import org.junit.Test;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.io.FileWriter;
 import java.nio.file.Files;
-import org.gradle.testkit.runner.GradleRunner;
-import org.gradle.testkit.runner.BuildResult;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  * A simple functional test for the 'org.openapitools.openapistylevalidator' plugin.
@@ -18,11 +17,25 @@ public class OpenapiStyleValidatorGradlePluginPluginFunctionalTest {
         // Setup the test build
         File projectDir = new File("build/functionalTest");
         Files.createDirectories(projectDir.toPath());
+        writeString(new File(projectDir, "openapi.yaml"), "" +
+                "openapi: 3.0.1\n" + 
+                "info:\n" + 
+                "  title: ping test\n" + 
+                "  version: '1.0'\n" + 
+                "servers:\n" + 
+                "  - url: 'http://localhost:9999/'\n" + 
+                "paths:\n" + 
+                "  /ping:\n" + 
+                "    post:\n" + 
+                "      operationId: pingGet\n" + 
+                "      responses:\n" + 
+                "        '201':\n" + 
+                "          description: OK");
         writeString(new File(projectDir, "settings.gradle"), "");
         writeString(new File(projectDir, "build.gradle"),
-            "plugins {" +
-            "  id('org.openapitools.openapistylevalidator')" +
-            "}");
+            "plugins {\n" +
+            "  id('org.openapitools.openapistylevalidator')\n" +
+            "}\n");
 
         // Run the build
         GradleRunner runner = GradleRunner.create();
@@ -33,7 +46,7 @@ public class OpenapiStyleValidatorGradlePluginPluginFunctionalTest {
         BuildResult result = runner.build();
 
         // Verify the result
-        assertTrue(result.getOutput().contains("Hello from plugin 'org.openapitools.openapistylevalidator'"));
+        // assertTrue(result.getOutput().contains("Hello from plugin 'org.openapitools.openapistylevalidator'"));
     }
 
     private void writeString(File file, String string) throws IOException {
--- gradle-plugin/src/main/java/org/openapitools/openapistylevalidator/gradle/OpenapiStyleValidatorTask.java
+++ gradle-plugin/src/main/java/org/openapitools/openapistylevalidator/gradle/OpenapiStyleValidatorTask.java
@@ -1,11 +1,45 @@
 package org.openapitools.openapistylevalidator.gradle;
 
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.parser.core.models.ParseOptions;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
+
 import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.options.Option;
+import org.openapitools.empoa.swagger.core.internal.SwAdapter;
+import org.openapitools.openapistylevalidator.OpenApiSpecStyleValidator;
 import org.openapitools.openapistylevalidator.ValidatorParameters;
 import org.openapitools.openapistylevalidator.ValidatorParameters.NamingStrategy;
+import org.openapitools.openapistylevalidator.styleerror.StyleError;
+
+import java.util.List;
 
 public class OpenapiStyleValidatorTask extends DefaultTask {
+
+    public static final String INPUT_FILE = "inputFile";
+
+    public static final String VALIDATE_INFO_LICENSE = "validateInfoLicense";
+    public static final String VALIDATE_INFO_DESCRIPTION = "validateInfoDescription";
+    public static final String VALIDATE_INFO_CONTACT = "validateInfoContact";
+
+    public static final String VALIDATE_OPERATION_OPERATION_ID = "validateOperationOperationId";
+    public static final String VALIDATE_OPERATION_DESCRIPTION = "validateOperationDescription";
+    public static final String VALIDATE_OPERATION_TAG = "validateOperationTag";
+    public static final String VALIDATE_OPERATION_SUMMARY = "validateOperationSummary";
+
+    public static final String VALIDATE_MODEL_PROPERTIES_EXAMPLE = "validateModelPropertiesExample";
+    public static final String VALIDATE_MODEL_NO_LOCAL_DEF = "validateModelNoLocalDef";
+
+    public static final String VALIDATE_NAMING = "validateNaming";
+    public static final String IGNORE_HEADER_X_NAMING = "ignoreHeaderXNaming";
+    public static final String PATH_NAMING_STRATEGY = "pathNamingStrategy";
+    public static final String PARAMETER_NAMING_STRATEGY = "parameterNamingStrategy";
+    public static final String PROPERTY_NAMING_STRATEGY = "propertyNamingStrategy";
+
+    
+    private String inputFile;
 
     private boolean validateInfoLicense = true;
     private boolean validateInfoDescription = true;
@@ -32,25 +66,106 @@ public class OpenapiStyleValidatorTask extends DefaultTask {
 
     @TaskAction
     public void execute() {
-        System.out.println("Hello from plugin 'org.openapitools.openapistylevalidator'");
+        if(inputFile == null) {
+            throw new GradleException("The input file is not defined");
+        }
+        getLogger().quiet("Validating spec: " + inputFile);
 
-//         OpenAPIParser openApiParser = new OpenAPIParser();
-//         ParseOptions parseOptions = new ParseOptions();
-//         parseOptions.setResolve(true);
-//
-//         SwaggerParseResult parserResult = openApiParser.readLocation(optionManager.getSource(commandLine), null, parseOptions);
-//         io.swagger.v3.oas.models.OpenAPI swaggerOpenAPI = parserResult.getOpenAPI();
-//
-//        // org.eclipse.microprofile.openapi.models.OpenAPI openAPI = SwAdapter.toOpenAPI(swaggerOpenAPI);
-//         OpenApiSpecStyleValidator openApiSpecStyleValidator = new OpenApiSpecStyleValidator(openAPI);
-//
-//        // ValidatorParameters parameters = createValidatorParameters());
-//
-//        openApiSpecStyleValidator.validate(parameters)
+        OpenAPIParser openApiParser = new OpenAPIParser();
+        ParseOptions parseOptions = new ParseOptions();
+        parseOptions.setResolve(true);
 
+        SwaggerParseResult parserResult = openApiParser.readLocation(inputFile, null, parseOptions);
+        io.swagger.v3.oas.models.OpenAPI swaggerOpenAPI = parserResult.getOpenAPI();
+
+        org.eclipse.microprofile.openapi.models.OpenAPI openAPI = SwAdapter.toOpenAPI(swaggerOpenAPI);
+        OpenApiSpecStyleValidator openApiSpecStyleValidator = new OpenApiSpecStyleValidator(openAPI);
+
+        ValidatorParameters parameters = createValidatorParameters();
+        List<StyleError> result = openApiSpecStyleValidator.validate(parameters);
+        if(!result.isEmpty()) {
+            getLogger().error("OpenAPI Specification does not meet the requirements. Issues:\n");
+            result.stream().map(StyleError::toString).forEach(m -> getLogger().error("\t" + m));
+            throw new GradleException("OpenAPI Style validation failed");
+        }
     }
 
-     public ValidatorParameters createValidatorParameters() {
+    @Option(option = INPUT_FILE, description = "")
+    public void setInputFile(String inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    @Option(option = VALIDATE_INFO_LICENSE, description = "")
+    public void setValidateInfoLicense(boolean validateInfoLicense) {
+        this.validateInfoLicense = validateInfoLicense;
+    }
+
+    @Option(option = VALIDATE_INFO_DESCRIPTION, description = "")
+    public void setValidateInfoDescription(boolean validateInfoDescription) {
+        this.validateInfoDescription = validateInfoDescription;
+    }
+
+    @Option(option = VALIDATE_INFO_CONTACT, description = "")
+    public void setValidateInfoContact(boolean validateInfoContact) {
+        this.validateInfoContact = validateInfoContact;
+    }
+
+    @Option(option = VALIDATE_OPERATION_OPERATION_ID, description = "")
+    public void setValidateOperationOperationId(boolean validateOperationOperationId) {
+        this.validateOperationOperationId = validateOperationOperationId;
+    }
+
+    @Option(option = VALIDATE_OPERATION_DESCRIPTION, description = "")
+    public void setValidateOperationDescription(boolean validateOperationDescription) {
+        this.validateOperationDescription = validateOperationDescription;
+    }
+
+    @Option(option = VALIDATE_OPERATION_TAG, description = "")
+    public void setValidateOperationTag(boolean validateOperationTag) {
+        this.validateOperationTag = validateOperationTag;
+    }
+
+    @Option(option = VALIDATE_OPERATION_SUMMARY, description = "")
+    public void setValidateOperationSummary(boolean validateOperationSummary) {
+        this.validateOperationSummary = validateOperationSummary;
+    }
+
+    @Option(option = VALIDATE_MODEL_PROPERTIES_EXAMPLE, description = "")
+    public void setValidateModelPropertiesExample(boolean validateModelPropertiesExample) {
+        this.validateModelPropertiesExample = validateModelPropertiesExample;
+    }
+
+    @Option(option = VALIDATE_MODEL_NO_LOCAL_DEF, description = "")
+    public void setValidateModelNoLocalDef(boolean validateModelNoLocalDef) {
+        this.validateModelNoLocalDef = validateModelNoLocalDef;
+    }
+
+    @Option(option = VALIDATE_NAMING, description = "")
+    public void setValidateNaming(boolean validateNaming) {
+        this.validateNaming = validateNaming;
+    }
+
+    @Option(option = IGNORE_HEADER_X_NAMING, description = "")
+    public void setIgnoreHeaderXNaming(boolean ignoreHeaderXNaming) {
+        this.ignoreHeaderXNaming = ignoreHeaderXNaming;
+    }
+
+    @Option(option = PATH_NAMING_STRATEGY, description = "")
+    public void setPathNamingStrategy(NamingStrategy pathNamingStrategy) {
+        this.pathNamingStrategy = pathNamingStrategy;
+    }
+
+    @Option(option = PARAMETER_NAMING_STRATEGY, description = "")
+    public void setParameterNamingStrategy(NamingStrategy parameterNamingStrategy) {
+        this.parameterNamingStrategy = parameterNamingStrategy;
+    }
+
+    @Option(option = PROPERTY_NAMING_STRATEGY, description = "")
+    public void setPropertyNamingStrategy(NamingStrategy propertyNamingStrategy) {
+        this.propertyNamingStrategy = propertyNamingStrategy;
+    }
+
+    public ValidatorParameters createValidatorParameters() {
         ValidatorParameters parameters = new ValidatorParameters();
         parameters.setValidateInfoLicense(validateInfoLicense);
         parameters.setValidateInfoDescription(validateInfoDescription);
