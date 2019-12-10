ed287adc5003eccfc8f3cac004847a733763a292
Jeremie Bresson <jeremie.bresson@unblu.com>
2019-12-10 07:29:29 +0100

    Rename NamingStrategy to NamingConvention & Document

--- gradle-plugin/src/main/java/org/openapitools/openapistylevalidator/gradle/OpenapiStyleValidatorTask.java
+++ gradle-plugin/src/main/java/org/openapitools/openapistylevalidator/gradle/OpenapiStyleValidatorTask.java
@@ -11,7 +11,7 @@ import org.gradle.api.tasks.options.Option;
 import org.openapitools.empoa.swagger.core.internal.SwAdapter;
 import org.openapitools.openapistylevalidator.OpenApiSpecStyleValidator;
 import org.openapitools.openapistylevalidator.ValidatorParameters;
-import org.openapitools.openapistylevalidator.ValidatorParameters.NamingStrategy;
+import org.openapitools.openapistylevalidator.ValidatorParameters.NamingConvention;
 import org.openapitools.openapistylevalidator.styleerror.StyleError;
 
 import java.util.List;
@@ -34,11 +34,10 @@ public class OpenapiStyleValidatorTask extends DefaultTask {
 
     public static final String VALIDATE_NAMING = "validateNaming";
     public static final String IGNORE_HEADER_X_NAMING = "ignoreHeaderXNaming";
-    public static final String PATH_NAMING_STRATEGY = "pathNamingStrategy";
-    public static final String PARAMETER_NAMING_STRATEGY = "parameterNamingStrategy";
-    public static final String PROPERTY_NAMING_STRATEGY = "propertyNamingStrategy";
+    public static final String PATH_NAMING_CONVENTION = "pathNamingConvention";
+    public static final String PARAMETER_NAMING_CONVENTION = "parameterNamingConvention";
+    public static final String PROPERTY_NAMING_CONVENTION = "propertyNamingConvention";
 
-    
     private String inputFile;
 
     private boolean validateInfoLicense = true;
@@ -55,9 +54,9 @@ public class OpenapiStyleValidatorTask extends DefaultTask {
 
     private boolean validateNaming = true;
     private boolean ignoreHeaderXNaming = true;
-    private NamingStrategy pathNamingStrategy = NamingStrategy.HyphenCase;
-    private NamingStrategy parameterNamingStrategy = NamingStrategy.CamelCase;
-    private NamingStrategy propertyNamingStrategy = NamingStrategy.CamelCase;
+    private NamingConvention pathNamingConvention = NamingConvention.HyphenCase;
+    private NamingConvention parameterNamingConvention = NamingConvention.CamelCase;
+    private NamingConvention propertyNamingConvention = NamingConvention.CamelCase;
     
     public OpenapiStyleValidatorTask() {
         this.setGroup("Verification");
@@ -90,57 +89,57 @@ public class OpenapiStyleValidatorTask extends DefaultTask {
         }
     }
 
-    @Option(option = INPUT_FILE, description = "")
+    @Option(option = INPUT_FILE, description = "OpenAPI specification being validated")
     public void setInputFile(String inputFile) {
         this.inputFile = inputFile;
     }
 
-    @Option(option = VALIDATE_INFO_LICENSE, description = "")
+    @Option(option = VALIDATE_INFO_LICENSE, description = "Ensures that there is a license section in the info section")
     public void setValidateInfoLicense(boolean validateInfoLicense) {
         this.validateInfoLicense = validateInfoLicense;
     }
 
-    @Option(option = VALIDATE_INFO_DESCRIPTION, description = "")
+    @Option(option = VALIDATE_INFO_DESCRIPTION, description = "Ensures that there is a description attribute in the info section")
     public void setValidateInfoDescription(boolean validateInfoDescription) {
         this.validateInfoDescription = validateInfoDescription;
     }
 
-    @Option(option = VALIDATE_INFO_CONTACT, description = "")
+    @Option(option = VALIDATE_INFO_CONTACT, description = "Ensures that there is a contact section in the info section")
     public void setValidateInfoContact(boolean validateInfoContact) {
         this.validateInfoContact = validateInfoContact;
     }
 
-    @Option(option = VALIDATE_OPERATION_OPERATION_ID, description = "")
+    @Option(option = VALIDATE_OPERATION_OPERATION_ID, description = "Ensures that there is an operation id for each operation")
     public void setValidateOperationOperationId(boolean validateOperationOperationId) {
         this.validateOperationOperationId = validateOperationOperationId;
     }
 
-    @Option(option = VALIDATE_OPERATION_DESCRIPTION, description = "")
+    @Option(option = VALIDATE_OPERATION_DESCRIPTION, description = "Ensures that there is a description for each operation")
     public void setValidateOperationDescription(boolean validateOperationDescription) {
         this.validateOperationDescription = validateOperationDescription;
     }
 
-    @Option(option = VALIDATE_OPERATION_TAG, description = "")
+    @Option(option = VALIDATE_OPERATION_TAG, description = "Ensures that there is a tag for each operation")
     public void setValidateOperationTag(boolean validateOperationTag) {
         this.validateOperationTag = validateOperationTag;
     }
 
-    @Option(option = VALIDATE_OPERATION_SUMMARY, description = "")
+    @Option(option = VALIDATE_OPERATION_SUMMARY, description = "Ensures that there is a summary for each operation")
     public void setValidateOperationSummary(boolean validateOperationSummary) {
         this.validateOperationSummary = validateOperationSummary;
     }
 
-    @Option(option = VALIDATE_MODEL_PROPERTIES_EXAMPLE, description = "")
+    @Option(option = VALIDATE_MODEL_PROPERTIES_EXAMPLE, description = "Ensures that the properties of the Schemas have an example value defined")
     public void setValidateModelPropertiesExample(boolean validateModelPropertiesExample) {
         this.validateModelPropertiesExample = validateModelPropertiesExample;
     }
 
-    @Option(option = VALIDATE_MODEL_NO_LOCAL_DEF, description = "")
+    @Option(option = VALIDATE_MODEL_NO_LOCAL_DEF, description = "Not implemented yet")
     public void setValidateModelNoLocalDef(boolean validateModelNoLocalDef) {
         this.validateModelNoLocalDef = validateModelNoLocalDef;
     }
 
-    @Option(option = VALIDATE_NAMING, description = "")
+    @Option(option = VALIDATE_NAMING, description = "Ensures the names follow a given naming convention")
     public void setValidateNaming(boolean validateNaming) {
         this.validateNaming = validateNaming;
     }
@@ -150,19 +149,19 @@ public class OpenapiStyleValidatorTask extends DefaultTask {
         this.ignoreHeaderXNaming = ignoreHeaderXNaming;
     }
 
-    @Option(option = PATH_NAMING_STRATEGY, description = "")
-    public void setPathNamingStrategy(NamingStrategy pathNamingStrategy) {
-        this.pathNamingStrategy = pathNamingStrategy;
+    @Option(option = PATH_NAMING_CONVENTION, description = "Naming convention for paths")
+    public void setPathNamingConvention(NamingConvention pathNamingConvention) {
+        this.pathNamingConvention = pathNamingConvention;
     }
 
-    @Option(option = PARAMETER_NAMING_STRATEGY, description = "")
-    public void setParameterNamingStrategy(NamingStrategy parameterNamingStrategy) {
-        this.parameterNamingStrategy = parameterNamingStrategy;
+    @Option(option = PARAMETER_NAMING_CONVENTION, description = "Naming convention for parameters")
+    public void setParameterNamingConvention(NamingConvention parameterNamingConvention) {
+        this.parameterNamingConvention = parameterNamingConvention;
     }
 
-    @Option(option = PROPERTY_NAMING_STRATEGY, description = "")
-    public void setPropertyNamingStrategy(NamingStrategy propertyNamingStrategy) {
-        this.propertyNamingStrategy = propertyNamingStrategy;
+    @Option(option = PROPERTY_NAMING_CONVENTION, description = "Naming convention for properties")
+    public void setPropertyNamingConvention(NamingConvention propertyNamingConvention) {
+        this.propertyNamingConvention = propertyNamingConvention;
     }
 
     public ValidatorParameters createValidatorParameters() {
@@ -178,9 +177,9 @@ public class OpenapiStyleValidatorTask extends DefaultTask {
         parameters.setValidateModelNoLocalDef(validateModelNoLocalDef);
         parameters.setValidateNaming(validateNaming);
         parameters.setIgnoreHeaderXNaming(ignoreHeaderXNaming);
-        parameters.setPathNamingStrategy(pathNamingStrategy);
-        parameters.setParameterNamingStrategy(parameterNamingStrategy);
-        parameters.setPropertyNamingStrategy(propertyNamingStrategy);
+        parameters.setPathNamingConvention(pathNamingConvention);
+        parameters.setParameterNamingConvention(parameterNamingConvention);
+        parameters.setPropertyNamingConvention(propertyNamingConvention);
         return parameters;
      }
 
